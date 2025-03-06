f8e9b7567dccff5db294d746ccf5fc046507b938
Rajeev <rajeevranjan9560807144@gmail.com>
2025-03-06 18:56:51 +0530

    updating the table

--- DSA/main.py
+++ DSA/main.py
@@ -1,8 +1,28 @@
-data =[
-     ["S.No", "Question", "Link", "Company"],
-    [1, "Equilibrium Point " , " https://www.geeksforgeeks.org/problems/equilibrium-point-1587115620/1?page=1&category=Arrays&sortBy=submissions" , " Amazon,Adobe,Google,Facebook,Microsoft,Uber, " ]
+data = [
+    ["S.No", "Question", "Link", "Company"],
+    [1, "Equilibrium Point", "https://www.geeksforgeeks.org/problems/equilibrium-point-1587115620/1?page=1&category=Arrays&sortBy=submissions", 
+     "Amazon, Adobe, Google, Facebook, Microsoft, Uber"]
 ]
 
-with open("C:\DSA_sheet\DSA\Arrays\Prefix_sum\Questions.txt","w") as file:
-    for row in data:
-      file.write("{:<5} {:<10} {:<5} {:<15}\n".format(*row))
+# Define column widths dynamically based on content
+col_widths = [max(len(str(row[i])) for row in data) for i in range(len(data[0]))]
+
+# Function to create a horizontal boundary
+def create_boundary():
+    return "+".join(["-" * (w + 2) for w in col_widths])
+
+# Writing the table to a file
+with open("DSA/Arrays/Prefix_sum/Questions.txt", "w", encoding="utf-8") as file:
+    top_border = "+" + create_boundary() + "+"
+    file.write(top_border + "\n")
+
+    # Writing data
+    for i, row in enumerate(data):
+        file.write("| " + " | ".join(f"{str(item):<{col_widths[j]}}" for j, item in enumerate(row)) + " |\n")
+        if i == 0:  # Header separator
+            file.write("|" + create_boundary() + "|\n")
+    
+    bottom_border = "+" + create_boundary() + "+"
+    file.write(bottom_border + "\n")
+
+print("Table written to table.txt successfully!")
